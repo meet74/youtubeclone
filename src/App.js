1eff4e576d22ae66f2f62a935a1b654d64d8a7ba
meet74 <meet237@gmail.com>
2022-06-17 17:49:03 +0530

    basic homepage done-1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css';
 import { Menu, SearchSharp, Mic, VideoCallOutlined, WidgetsOutlined, NotificationsNoneOutlined, YouTube } from '@mui/icons-material';
 import youtubeLogo from './assets/images/youtube_logo.png'
 import { Navbar } from './components/Navbar/Navbar';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 function App() {
   const [sideBarState, setsideBarState] = useState(false);
+  const [images, setimages] = useState([])
 
+  useEffect(() => {
+    
+    fetch('https://picsum.photos/v2/list',{
+      method:"GET"
+    }).then(async res => {
+      const apiImages = await res.json();
+      setimages(apiImages);
+    })
+  }, [])
+  
   const showSideBar = () => {
 
     setsideBarState(!sideBarState)}
@@ -56,7 +68,27 @@ function App() {
         <Navbar sideBarState={sideBarState}/>
         </div>
       <main className='main-body'>
-            <h1>Shreehari</h1>
+            <div className='grid-container'>
+              {
+                images.map((item,index) => {
+                  return(
+                   <div className='grid-item'>
+                      <picture>
+                      <img src={item.download_url} style={{height:"175px",width:"300px"}}/>
+                      <div className='length-text'>
+                      <p >1:45</p>
+                      </div>
+                      </picture>
+                      <div style={{display:"flex",alignItems:"center",justifyContent:"center"}}>
+                      <img src={item.download_url} style={{height:"30px",width:"30px",borderRadius:"50px"}}/>
+                      <p style={{fontFamily:"Roboto", fontSize:"1rem",textAlign:"start", margin:"5px 5px 5px 15px"}}>Use the Most popular Crosshair in Valorant</p>
+                      <MoreVertIcon style={{ color: "white", width: "25px", height: "25px" }}  />
+                      </div>
+                   </div>
+                  )
+                })
+              }
+            </div>
           </main>
       </div>
     </div>
--- src/components/Navbar/SidemenuItem.js
+++ src/components/Navbar/SidemenuItem.js
@@ -8,20 +8,20 @@ export const SidebarData = [
     {
       title: 'Home',
       path: '/',
-      icon: <HomeIcon style={{color:"white",width:"25px",height:"25px"}} />,
+      icon: <HomeIcon style={{color:"white",width:"20px",height:"20px"}} />,
       cName: 'nav-text'
     },
     {
       title: 'Trending',
       path: '/',
-      icon: <LocalFireDepartmentIcon style={{color:"white",width:"25px",height:"25px"}} />,
+      icon: <LocalFireDepartmentIcon style={{color:"white",width:"20px",height:"20px"}} />,
       cName: 'nav-text'
     },
 
     {
       title: 'Subscriptions',
       path: '/',
-      icon: <SubscriptionsIcon style={{color:"white",width:"25px",height:"25px"}} />,
+      icon: <SubscriptionsIcon style={{color:"white",width:"20px",height:"20px"}} />,
       cName: 'nav-text'
     },
 
